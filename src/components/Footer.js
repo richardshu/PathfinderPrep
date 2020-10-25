939fbb81a40648dcf6c0e8267c3760360af13407
Richard Shu <richardjshu@gmail.com>
2020-10-25 02:53:24 -0700

    Clean up footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,22 +3,33 @@ import "./Footer.css";
 
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faInstagram } from "@fortawesome/free-brands-svg-icons";
+import { faFacebook } from "@fortawesome/free-brands-svg-icons";
+import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
 
 class Footer extends Component {
   render() {
     return (
       <footer>
-        <a href="mailto:pathfinderpreparation@gmail.com">
-          pathfinderpreparation@gmail.com
-        </a>
-        <ul>
-          <li>
-            <a target="_blank" href="https://www.instagram.com/pathfinderprep/">
-              <FontAwesomeIcon icon={faInstagram} />
-              <span className="label"> Instagram</span>
-            </a>
-          </li>
-        </ul>
+        <div>
+          <h4>Get In Touch</h4>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.instagram.com/pathfinderprep/"
+          >
+            <FontAwesomeIcon className="footer-icon" icon={faInstagram} />
+          </a>
+          <a
+            target="_blank"
+            rel="noopener noreferrer"
+            href="https://www.facebook.com/Pathfinder-Prep-108531454364085"
+          >
+            <FontAwesomeIcon className="footer-icon" icon={faFacebook} />
+          </a>
+          <a href="mailto:pathfinderpreparation@gmail.com">
+            <FontAwesomeIcon className="footer-icon" icon={faEnvelope} />
+          </a>
+        </div>
       </footer>
     );
   }
