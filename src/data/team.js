7a77125292a8b109da725c0b3b7511090989b109
Richard Shu <richardjshu@gmail.com>
2021-01-09 21:03:49 -0800

    Add new tutor cards

--- src/data/team.js
+++ src/data/team.js
@@ -1,13 +1,8 @@
 export const TeamData = [
   {
-    id: "patrick-ma",
-    name: "Patrick Ma",
-    university: "UCLA '23",
-  },
-  {
-    id: "darren-chiu",
-    name: "Darren Chiu",
-    university: "Princeton '23",
+    id: "victor-chai",
+    name: "Victor Chai",
+    university: "Pomona College '23",
   },
   {
     id: "andrew-chang",
@@ -15,9 +10,9 @@ export const TeamData = [
     university: "Columbia & Juilliard '23",
   },
   {
-    id: "jason-zeng",
-    name: "Jason Zeng",
-    university: "UC Berkeley '23",
+    id: "darren-chiu",
+    name: "Darren Chiu",
+    university: "Princeton '23",
   },
   {
     id: "samantha-hong",
@@ -25,14 +20,54 @@ export const TeamData = [
     university: "Brown '23",
   },
   {
+    id: "karyn-how",
+    name: "Karyn How",
+    university: "UCLA '23",
+  },
+  {
+    id: "jeffrey-huynh",
+    name: "Jeffrey Huynh",
+    university: "UCLA '22",
+  },
+  {
+    id: "clara-kim",
+    name: "Clara Kim",
+    university: "Wellesley '23",
+  },
+  {
+    id: "jay-kuo",
+    name: "Jay Kuo",
+    university: "Vanderbilt '23",
+  },
+  {
+    id: "kevin-lin",
+    name: "Kevin Lin",
+    university: "NYU '22",
+  },
+  {
+    id: "lorna-louie",
+    name: "Lorna Louie",
+    university: "NYU Shanghai '23",
+  },
+  {
+    id: "patrick-ma",
+    name: "Patrick Ma",
+    university: "UCLA '23",
+  },
+  {
     id: "isaac-no",
     name: "Isaac No",
     university: "UCLA '23",
   },
   {
-    id: "victor-chai",
-    name: "Victor Chai",
-    university: "Pomona College '23",
+    id: "brian-peh",
+    name: "Brian Peh",
+    university: "Cal State Fullerton '23",
+  },
+  {
+    id: "doah-shin",
+    name: "Doah Shin",
+    university: "Vanderbilt '23",
   },
   {
     id: "richard-shu",
@@ -40,9 +75,19 @@ export const TeamData = [
     university: "NYU '20",
   },
   {
-    id: "jeffrey-huynh",
-    name: "Jeffrey Huynh",
-    university: "UCLA '22",
+    id: "jonathan-tan",
+    name: "Jonathan Tan",
+    university: "UCLA '24",
+  },
+  {
+    id: "jason-zeng",
+    name: "Jason Zeng",
+    university: "UC Berkeley '23",
+  },
+  {
+    id: "annika-zhang",
+    name: "Annika Zhang",
+    university: "UCLA '23",
   },
   {
     id: "david-zhou",
